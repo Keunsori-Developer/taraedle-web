fa2e9af8a7c3b51c6bacd9167e431d085c33d7e1
mji4209 <mji4209@naver.com>
2025-03-06 09:29:55 +0900

    하루마다 localstorage 초기화 테스트

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css';
-import React, { lazy } from 'react';
+import React, { lazy, useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const ProblemPage = lazy(() => import('./page/ProblemPage'));
@@ -7,7 +7,28 @@ import { MainPage } from './page/MainPage';
 import { LoginPage } from './page/LoginPage';
 import { CallbackPage } from './page/CallbackPage';
 
+const resetLocalStorage = () => {
+  const now = new Date();
+
+  const hours = now.getHours();
+  const minutes = now.getMinutes();
+  const seconds = now.getSeconds();
+
+  if (hours === 0 && minutes === 0 && seconds === 0) {
+    localStorage.clear();
+    window.location.reload();
+  }
+}
+
 function App() {
+  useEffect(() => {
+    const invervalId = setInterval(() => {
+      resetLocalStorage();
+    }, 1000);
+
+    return () => clearInterval(invervalId);
+  }, [])
+
   return (
     <div className="App">
       <BrowserRouter>
